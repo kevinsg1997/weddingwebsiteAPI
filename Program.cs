c384fdbe7e51b751f977cb80dc663d2dc24589f5
kevinsg1997 <kevinsg1997@gmail.com>
2025-08-19 23:42:49 -0300

    fix - port 8080

--- Program.cs
+++ Program.cs
@@ -40,6 +40,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://*:{port}");
 app.Run();
