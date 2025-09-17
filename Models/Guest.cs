d9f830e2bb4cf55c23394077c0c3cb3370769ef6
kevinsg1997 <kevinsg1997@gmail.com>
2025-09-17 18:53:53 -0300

    fix

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -9,13 +9,13 @@ namespace WeddingMerchantApi.Models
         public string Name { get; set; }
         public string? Email { get; set; }
         public bool IsGoing { get; set; }
-    }
     
-    public Guest(string name, bool isGoing)
-    {
-        Id = Guid.NewGuid();
-        CreatedAt = DateTime.UtcNow;
-        Name = name ?? throw new ArgumentNullException(nameof(name), "Name cannot be null");
-        IsGoing = isGoing;
+        public Guest(string name, bool isGoing)
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            Name = name ?? throw new ArgumentNullException(nameof(name), "Name cannot be null");
+            IsGoing = isGoing;
+        }
     }
 }
